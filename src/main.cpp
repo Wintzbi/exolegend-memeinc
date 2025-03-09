19729873f2dc512d82411c0fc6459f9ba62faebe
Wintzbi <leynaickd@gmail.com>
2025-03-09 01:45:38 +0100

    stage fuck it

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,13 @@
 #include "gladiator.h"
-#include <chrono>
+#include "Vector2.hpp"
+#include <vector>
+#include <array>
+#include <math.h>
+#include <cmath>
+#undef abs
 
-std::chrono::time_point<std::chrono::system_clock> start, end;
-float time_elapsed;
-int Num_tour=0;
+
 Gladiator *gladiator;
-void reset();
-void dropBomb();
-bool UpdateNearestBomb=true;
-Position LastBombToGet;
-Position targetBomb = { -1, -1 };
-#define MAX_BOMB 50
-Position BombPos[MAX_BOMB];
-
-// Constantes de contrôle
-
 
 float kw = 0.4f;
 float kv = 2.f;
@@ -23,12 +16,6 @@ float vlimit = 1.25;
 float erreurPos = 0.05;
 float angleThreshold=0.1;
 
-
-template <typename T1, typename T2>
-auto my_max(T1 a, T2 b) -> decltype(a + b)
-{
-    return (a > b) ? a : b;
-}
 double reductionAngle(double x)
 {
     x = fmod(x + PI, 2 * PI);
@@ -36,10 +23,198 @@ double reductionAngle(double x)
         x += 2 * PI;
     return x - PI;
 }
+
+void retreat()
+{
+    // Reculer pendant 0.5 secondes
+    gladiator->control->setWheelSpeed(WheelAxis::LEFT, -0.8);
+    gladiator->control->setWheelSpeed(WheelAxis::RIGHT, -0.8);
+    delay(800);
+
+    // Réinitialiser les variables ou effectuer d'autres actions nécessaires
+    // ...
+
+    // Reprendre la mission de base
+    /*Position myPosition = gladiator->robot->getData().position;
+    Position target = getNearestEnnemy();
+    if (target.x == 0 && target.y == 0)
+    {
+        target = {2.1, 1.8, 0};
+    }
+    if (limite_maze(target))
+    {
+        Vector2 goal = Vector2(target.x, target.y);
+        bool showLogs = false;
+        aim(gladiator, goal, showLogs);
+    }*/
+}
+void arme_fou(int duree)
+{
+    gladiator->log("Appel de la fonction arme fou");
+    gladiator->control->setWheelSpeed(WheelAxis::LEFT, 1);
+    gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 1);
+    while (duree > 0)
+    {
+
+        gladiator->weapon->setTarget(WeaponPin::M1, 135);
+        delay(100);
+        gladiator->weapon->setTarget(WeaponPin::M1, 50);
+        delay(100);
+        duree -= 200;
+    }
+    gladiator->weapon->setTarget(WeaponPin::M1, 95);
+}
 inline float moduloPi(float a) // return angle in [-pi; pi]
 {
     return (a < 0.0) ? (std::fmod(a - M_PI, 2 * M_PI) + M_PI) : (std::fmod(a + M_PI, 2 * M_PI) - M_PI);
 }
+inline bool aim(Gladiator *gladiator, const Vector2 &target, bool showLogs)
+{
+    constexpr float ANGLE_REACHED_THRESHOLD = 0.05;
+    constexpr float POS_REACHED_THRESHOLD = 0.20;
+
+    auto posRaw = gladiator->robot->getData().position;
+    Vector2 pos{posRaw.x, posRaw.y};
+
+    Vector2 posError = target - pos;
+
+    float targetAngle = posError.angle();
+    float angleError = moduloPi(targetAngle - posRaw.a);
+
+    bool targetReached = false;
+    float leftCommand = 0.f;
+    float rightCommand = 0.f;
+
+    if (posError.norm2() < POS_REACHED_THRESHOLD) //
+    {
+        arme_fou(200);
+        //delay(400);
+        retreat();
+    }
+
+    else if (std::abs(angleError) > ANGLE_REACHED_THRESHOLD)
+    {
+        float factor = 0.35;
+        if (angleError < 0)
+            factor = -factor;
+        rightCommand = factor;
+        leftCommand = -factor;
+        gladiator->control->setWheelSpeed(WheelAxis::LEFT, leftCommand);
+        gladiator->control->setWheelSpeed(WheelAxis::RIGHT, rightCommand);
+    }
+    else
+    {
+        float factor = 0.75;
+        rightCommand = factor + angleError * 0.1; //+angleError*0.1  => terme optionel, "pseudo correction angulaire";
+        leftCommand = factor - angleError * 0.1;  //-angleError*0.1   => terme optionel, "pseudo correction angulaire";
+        gladiator->control->setWheelSpeed(WheelAxis::LEFT, leftCommand);
+        gladiator->control->setWheelSpeed(WheelAxis::RIGHT, rightCommand);
+    }
+
+    if (showLogs || targetReached)
+    {
+        gladiator->log("ta %f, ca %f, ea %f, tx %f cx %f ex %f ty %f cy %f ey %f", targetAngle, posRaw.a, angleError, target.x(), pos.x(), posError.x(), target.y(), pos.y(), posError.y());
+    }
+
+    return targetReached;
+}
+
+/******************************************/
+
+struct PositionEx
+{
+    Vector2 pos;
+    bool isEnnemy;
+    int life_ennemies;
+};
+
+std::vector<PositionEx> ennemies{};
+
+std::array<PositionEx, 4>
+fetchEnnemyRobotsData()
+{
+    std::array<PositionEx, 4> res{};
+    RobotList robotsIds = gladiator->game->getPlayingRobotsId();
+    for (int i = 0; i < 4; ++i)
+    {
+        RobotData other = gladiator->game->getOtherRobotData(robotsIds.ids[i]);
+        res.at(i).isEnnemy = other.teamId != gladiator->robot->getData().teamId;
+        res.at(i).pos = {other.position.x, other.position.y};
+        res.at(i).life_ennemies = other.lifes;
+    }
+    return res;
+}
+
+PositionEx findNearest(std::vector<PositionEx> ennemies) // renvoie la chose la plus proche
+{
+    float min = 100000.f;
+    PositionEx nearest{};
+    const Position robotPos = gladiator->robot->getData().position;
+    for (auto ennemy : ennemies)
+    {
+        const float dist = std::sqrt((robotPos.x - ennemy.pos.x()) * (robotPos.x - ennemy.pos.x()) + (robotPos.y - ennemy.pos.y()) * (robotPos.y - ennemy.pos.y()));
+        if (dist < min)
+        {
+            min = dist;
+            nearest = ennemy;
+        }
+    }
+    return nearest;
+}
+
+Position getNearestEnnemy()
+{ // renvoie l'énemie le plus proche
+    ennemies.clear();
+    const std::array<PositionEx, 4> robots = fetchEnnemyRobotsData();
+    for (auto rpos : robots)
+    {
+        if (rpos.isEnnemy && rpos.life_ennemies)
+        {
+            // gladiator->log("état vie adverse :%f", rpos.life_ennemies);
+            ennemies.push_back(rpos);
+        }
+    }
+
+    if (ennemies.size() > 0)
+    {
+        // gladiator->log("ennemies size :%f", ennemies.size());
+        PositionEx nearestEnnemy = findNearest(ennemies);
+        return Position{nearestEnnemy.pos.x(), nearestEnnemy.pos.y()};
+    }
+    return {0.f};
+}
+
+bool limite_maze(Position target)
+{
+    if ((float)target.x > 0 &&
+        (float)target.x < gladiator->maze->getSize() &&
+        (float)target.y > 0 && (float)target.y < gladiator->maze->getSize())
+    {
+        gladiator->log("Cible dans le terrain");
+        return true;
+    }
+
+    return false; // Added this line
+}
+
+void reset()
+{
+    // fonction de reset:
+    // initialisation de toutes vos variables avant le début d'un match
+    gladiator->log("Call of reset function"); // GFA 4.5.1
+}
+
+void setup()
+{
+    // instanciation de l'objet gladiator
+    gladiator = new Gladiator();
+    // enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
+    gladiator->game->onReset(&reset); // GFA 4.4.1
+    gladiator->weapon->initWeapon(WeaponPin::M1, WeaponMode::SERVO);
+     gladiator->weapon->setTarget(WeaponPin::M1, 90);
+    // gladiator->game->enableFreeMode(RemoteMode::OFF); // GFA 4.3.2
+}
+/******************************************/
 
 
 void go_to(Position cons, Position pos)
@@ -83,7 +258,6 @@ void go_to(Position cons, Position pos)
         // Si la position est proche de la cible, arrêter le robot
         consvr = 0;
         consvl = 0;
-        UpdateNearestBomb = true;
     }
 
     // Appliquer les vitesses aux roues
@@ -91,222 +265,24 @@ void go_to(Position cons, Position pos)
     gladiator->control->setWheelSpeed(WheelAxis::LEFT, consvl, false);  // GFA 3.2.1
 }
 
-void convert(unsigned int i, unsigned int j) {
-    float squareSize = gladiator->maze->getSquareSize();
-
-    Position centerCoor;
-
-    centerCoor.x = (i + 0.5) * squareSize;
-    centerCoor.y = (j + 0.5) * squareSize;
-
-    Position myPosition = gladiator->robot->getData().position;
-    Position goal{centerCoor.x, centerCoor.y, 0};
-    go_to(goal, myPosition);
-}
-
-bool CheckFuturCase(int i, int j){
-    Num_tour = floor((time_elapsed+6 ) / 20.f); // Arrondi à l'inférieur
-
-    // Récupérer la taille réelle du labyrinthe en cases
-    float squareSize = gladiator->maze->getSquareSize();
-    float MazeSize = gladiator->maze->getCurrentMazeSize();
-    int MazeSize_int = floor(MazeSize / squareSize)-1;
-
-    // Log pour débogage
-    gladiator->log("TIme : %f | Numéro tour %d | MazeSize %d | Limites : (%d, %d)",time_elapsed, Num_tour,MazeSize_int,Num_tour,11 - Num_tour);
-
-    // Vérification des limites du labyrinthe
-    if (i <= Num_tour-1 || i >= 12 - Num_tour  || j <= Num_tour-1 || j >= 12 - Num_tour ) {
-        //gladiator->log("Bombe or limite");
-        return false;  // Ne pas explorer cette case
-    }
-    return true;  // La case est valide
-}
-
-void BombListing() {
-    int index = 0;
-
-    // Réinitialiser la liste des bombes
-    for (int i = 0; i < MAX_BOMB; i++) {
-        BombPos[i].x = -1;
-        BombPos[i].y = -1;
-    }
-    float squareSize = gladiator->maze->getSquareSize();
-
-    // Parcours du labyrinthe (ajustez la taille si nécessaire)
-    float MazeSize = gladiator->maze->getCurrentMazeSize();
-    int MazeSize_int = static_cast<int>(MazeSize / squareSize);
-    gladiator->log( "MazeSize %d | Limites : (%d, %d)",MazeSize_int,Num_tour,11 - Num_tour);
-
-    for (int i = 0; i < 12; i++) {
-        for (int j = 0; j < 12; j++) {
-            const MazeSquare* indexedSquare = gladiator->maze->getSquare(i, j);
-            Coin coin = indexedSquare->coin;
-            int danger = indexedSquare->danger;
-            if(coin.value<1){
-                continue;
-            }
-            // Calcul de la position réelle de la bombe
-            int i_bomb = static_cast<int>((coin.p.x / squareSize) - 0.5);
-            int j_bomb = static_cast<int>((coin.p.y / squareSize) - 0.5);
-            
-                // Vérification si la case est une limite
-                if (!CheckFuturCase(i_bomb, j_bomb) ) {
-                gladiator->log("Bombe hors limites (%d, %d) ", i_bomb, j_bomb);
-
-                continue;  // Ignorer les cases sur les bords
-            }
-            
-            
-
-            // Vérification si la bombe est valide
-            gladiator->log("Donnée de filtres(%d, %d) ", coin.value , danger);
-
-            if (coin.value > 0 && danger < 1) {
-                Position posCoin = coin.p;
-                if (index < MAX_BOMB) {
-                    BombPos[index] = posCoin;  // Ajouter la bombe à la liste
-                    index++;  // Incrémenter l'indice
-                }
-            }
-        }
-    }
-}
-
-
-Position FindNearestBomb(){
-    RobotData myData = gladiator->robot->getData();
-    double minDistance = 9999;
-    int compteurBombes =0;
-    BombListing();
-    for (int i = 0; i < MAX_BOMB; i++) {
-        if (BombPos[i].x != -1 && BombPos[i].y != -1) {
-            compteurBombes++;
-            double dx = BombPos[i].x - myData.position.x;
-            double dy = BombPos[i].y - myData.position.y;
-            double distance = sqrt(dx * dx + dy * dy);
-            
-            if (distance < minDistance) {
-                minDistance = distance;
-                targetBomb = BombPos[i];
-                
-            }
-        }
-    }
-        float squareSize = gladiator->maze->getSquareSize();
-
-    // Calcul de la position réelle de la bombe
-    int i_bomb = static_cast<int>((targetBomb.x / squareSize) - 0.5);
-    int j_bomb = static_cast<int>((targetBomb.y / squareSize) - 0.5);
-
-    int myPOs_i = static_cast<int>((myData.position.x / squareSize) - 0.5);
-    int myPOs_j = static_cast<int>((myData.position.y / squareSize) - 0.5);
-    gladiator->log("Bombs detecte : %d | Nearest bomb at (%d, %d)| Mypos : (%d,%d) | distance: %f", compteurBombes,i_bomb, j_bomb, myPOs_i,myPOs_j,minDistance);
-
-    return targetBomb;
-}
-
-bool avoidDanger(const MazeSquare* neighbor, int value){
-    if(neighbor->danger < value ){
-        return 1;
-    }
-    return 0;
-}
-
-bool canGo(MazeSquare* neighbor){
-    if(neighbor != nullptr && avoidDanger(neighbor, 1)){
-        return 1;
-    }
-    return 0;
-}
-
-void setup()
-{
-    // instanciation de l'objet gladiator
-    gladiator = new Gladiator();
-    // enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
-    gladiator->game->onReset(&reset); // GFA 4.4.1
-}
-
-void reset()
-{
-
-    // fonction de reset:
-    // initialisation de toutes vos variables avant le début d'un match
-    gladiator->log("Call of reset function"); // GFA 4.5.1
-    start = std::chrono::system_clock::now();
-
-
-}
-void dropBomb(){
-        int bombcount = gladiator->weapon->getBombCount();
-        //gladiator->log("Nombre bombe : %d",bombcount);
-        if (gladiator->weapon->canDropBombs(1)) {
-            // Dropper une bombe
-            gladiator->weapon->dropBombs(bombcount);
-            //gladiator->log("Drop bomb");
-        }
-}
-
-void boom(const MazeSquare* nearestSquare, unsigned char teamId){
-    if (nearestSquare->possession != teamId && avoidDanger(nearestSquare,4)){
-        gladiator->log("TeamId: %u || Possession: %u", teamId, nearestSquare->possession);
-        dropBomb();
-    }
-}
-
-void CheckBombStatuts(){
-        float squareSize = gladiator->maze->getSquareSize();
-        
-        //Calcul position bombe
-        int i_bomb= (LastBombToGet.x/squareSize)-0.5;
-        int j_bomb= (LastBombToGet.y/squareSize)-0.5;
-        const MazeSquare *indexedSquare = gladiator->maze->getSquare(i_bomb, j_bomb);
-        Coin coin = indexedSquare->coin;
-        int danger =indexedSquare->danger;
-        //gladiator->log("Case visée: ( %d; %d ) , danger = %u", i_bomb, j_bomb,danger);
-   
-
-        if (coin.value < 1 || danger >2){
-                UpdateNearestBomb=true;
-            }
-}
-
-void flee(const MazeSquare* nearestSquare, Position position){
-
-    if(avoidDanger(nearestSquare, 3) != 1){
-        go_to({1.5,1.5,0}, position);
-    }
-}
-
 void loop()
-{   
-    if (gladiator->game->isStarted()) {
-
-        end = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end - start;
-        time_elapsed =elapsed_seconds.count();
-        //gladiator->log("Temps écoulé %f",time_elapsed);
-        RobotData myData = gladiator->robot->getData();
-        unsigned char teamId = myData.teamId;
-        const MazeSquare* nearestSquare = gladiator->maze->getNearestSquare();
-
-        boom(nearestSquare,teamId);
-
-        if (UpdateNearestBomb){
-                targetBomb=FindNearestBomb();
-        }
-        
-        if (targetBomb.x != -1 && targetBomb.y != -1) {
-            LastBombToGet=targetBomb;
-            UpdateNearestBomb=false;
-        }
-
-        CheckBombStatuts(); // vérifie si on peut toujours aller à la bombe target
-
+{
+    if (gladiator->game->isStarted())
+    { // tester si un match à déjà commencer
+        // code de votre stratégie
         Position myPosition = gladiator->robot->getData().position;
-        go_to({LastBombToGet.x,LastBombToGet.y,0},myPosition);
-        //gladiator->log("Tracking bomb at (%f, %f)", LastBombToGet.x, LastBombToGet.y);
-        delay(75);
+        Position target = getNearestEnnemy();
+        if (target.x == 0 && target.y == 0) // plus d'enemie
+        {
+            target = {1.5, 1.5, 0}; // rajouter code ley
+        }
+       if( limite_maze(target)){ //enemie dans la map
+        Position goal{target.x, target.y, 0};
+        go_to(goal, myPosition);}
+
+        // turn(target, myPosition);
+        // delay(500);
+        // go_to(target, myPosition);
     }
+    delay(10);
 }
